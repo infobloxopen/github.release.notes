594ca356f1e6d8563be21044a795f62a75207be5
Dzmitry Lahuta <dlahuta@infoblox.com>
2021-05-19 16:35:05 +0300

    Add service template

---
+++ cmd/server/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+
+	"github.com/infobloxopen/atlas-app-toolkit/server"
+
+	"github.com/infobloxopen/atlas-app-toolkit/gorm/resource"
+)
+
+func main() {
+	doneC := make(chan error)
+	logger := NewLogger()
+
+	go func() { doneC <- ServeExternal(logger) }()
+
+	if err := <-doneC; err != nil {
+		logger.Fatal(err)
+	}
+}
+
+func NewLogger() *logrus.Logger {
+	logger := logrus.StandardLogger()
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+	logger.SetReportCaller(true)
+
+	// Set the log level on the default logger based on command line flag
+	if level, err := logrus.ParseLevel(viper.GetString("logging.level")); err != nil {
+		logger.Errorf("Invalid %q provided for log level", viper.GetString("logging.level"))
+		logger.SetLevel(logrus.InfoLevel)
+	} else {
+		logger.SetLevel(level)
+	}
+
+	return logger
+}
+
+// ServeExternal builds and runs the server that listens on ServerAddress and GatewayAddress
+func ServeExternal(logger *logrus.Logger) error {
+
+	grpcServer, err := NewGRPCServer(logger)
+	if err != nil {
+		logger.Fatalln(err)
+	}
+
+	s, err := server.NewServer(
+		server.WithGrpcServer(grpcServer),
+	)
+	if err != nil {
+		logger.Fatalln(err)
+	}
+
+	grpcL, err := net.Listen("tcp", fmt.Sprintf("%s:%s", viper.GetString("server.address"), viper.GetString("server.port")))
+	if err != nil {
+		logger.Fatalln(err)
+	}
+
+	logger.Printf("serving gRPC at %s:%s", viper.GetString("server.address"), viper.GetString("server.port"))
+
+	return s.Serve(grpcL, nil)
+}
+
+func init() {
+	pflag.Parse()
+	viper.BindPFlags(pflag.CommandLine)
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AddConfigPath(viper.GetString("config.source"))
+	if viper.GetString("config.file") != "" {
+		log.Printf("Serving from configuration file: %s", viper.GetString("config.file"))
+		viper.SetConfigName(viper.GetString("config.file"))
+		if err := viper.ReadInConfig(); err != nil {
+			log.Fatalf("cannot load configuration: %v", err)
+		}
+	} else {
+		log.Printf("Serving from default values, environment variables, and/or flags")
+	}
+	resource.RegisterApplication(viper.GetString("app.id"))
+	resource.SetPlural()
+}
---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/infobloxopen/github.release.notes
+
+go 1.15
+
+require (
+	github.com/envoyproxy/protoc-gen-validate v0.6.1
+	github.com/golang/protobuf v1.5.2
+	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0
+	github.com/infobloxopen/atlas-app-toolkit v0.24.1
+	github.com/sirupsen/logrus v1.8.1
+	github.com/spf13/pflag v1.0.5
+	github.com/spf13/viper v1.7.1
+	google.golang.org/genproto v0.0.0-20210518161634-ec7691c0a37d
+	google.golang.org/grpc v1.37.1
+)
